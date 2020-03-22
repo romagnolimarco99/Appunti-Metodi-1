bc9ce7ea5bf2d0b5390a97aa02be520a153350ce
b.tomelleri <bernardo.tomelleri@gmail.com>
2020-03-22 20:26:15 +0100

    fixini

--- grapher.py
+++ grapher.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Mar 21 23:31:53 2020
+Created on Sat Mar 21 23:31:53 2020
 
 @author: berni
 """
-
 import numpy as np
 import cmath as cm
 from matplotlib import pyplot as plt
@@ -24,9 +23,9 @@ Nstep = 5
 def f_n(x, n):
     y_n = []
     for x in xx:
-        if a <= x <= c:
+        if a <= x <= c - 1./n:
             y_n.append(0)
-        elif c -1./n <= x <= c + 1./n:
+        elif (c - 1./n) <= x <= (c + 1./n):
             y_n.append(n/2. * (x - c + 1./n))
         elif c + 1./n <= x <= b:
             y_n.append(1)
@@ -45,7 +44,7 @@ def f(x):
     
 # Variabili di controllo dei grafici in ordine decrescente di pesantezza
 tex=True
-tick=False
+tick=True
 # Impostazioni opzionali per tipografia in LaTeX
 if tex:
    plt.rc('text', usetex=True)
@@ -54,23 +53,27 @@ if tex:
 # Grafico
 fig, ax = plt.subplots()
 plt.plot(xx, f(xx), label='$f(x)$', zorder = 10)
-plt.plot(xx, f_n(xx, 1), label='$f_1 (x)$')
+plt.plot(xx, f_n(xx, 1), ls='--', label='$f_1 (x)$')
 # for n in range(5, Niter, Nstep):
-for n in [2, 3, 4, 5, 10, 20, 30]:
-    plt.plot(xx, f_n(xx, n), label='$f_{%d}(x)$' %n)
-ax.grid(color = 'gray', ls = '--', alpha=0.7)
-ax.set_xlabel('x', x=0.9)
-ax.set_ylabel('y')
+for n in [2, 4, 10, 20]:
+    plt.plot(xx, f_n(xx, n), ls='--', label='$f_{%d}(x)$' %n)
+
 ax.minorticks_on()
+ax.grid(c = 'gray', ls = '--', alpha=0.7)
+ax.grid(which = 'minor', c = 'gray', ls = '--', alpha=0.4)
+ax.set_xlabel('$x$', x=0.9, fontsize=14)
+ax.set_ylabel('$f(x)$', y=0.8, rotation=0, fontsize=14)
 ax.tick_params(direction='in', length=5, width=1., top=True, right=True)
 ax.tick_params(which='minor', direction='in', width=1., top=True, right=True)
 if tick:
-    ax.set_xlim(0, c)
+    # ax.set_xlim(a, b)
     # ax.set_ylim(0, 1)
     ax.xaxis.set_major_locator(plt.MultipleLocator(0.2))
-    ax.xaxis.set_minor_locator(plt.MultipleLocator(5e-2))
+    plt.xticks([1, 2, 3], ['a', 'c', 'b'])
+    # ax.xaxis.set_minor_locator(plt.MultipleLocator(5e-2))
     ax.yaxis.set_major_locator(plt.MultipleLocator(0.5))
-    ax.yaxis.set_minor_locator(plt.MultipleLocator(0.1))
+    # ax.yaxis.set_minor_locator(plt.MultipleLocator(5e-2))
+    plt.tight_layout()
 
 legend = ax.legend(loc ='best')
 fig.show()
